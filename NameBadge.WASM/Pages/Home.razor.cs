c7fb3d325f504b4ad879222b55f9ba130cfd63bf
Lawrence (Drew) Whisenant <7390131+drewwhis@users.noreply.github.com>
2026-06-18 23:41:10 -0400

    add some welcome messaging

--- NameBadge.WASM/Pages/Home.razor.cs
+++ NameBadge.WASM/Pages/Home.razor.cs
@@ -1,8 +1,36 @@
 using Microsoft.AspNetCore.Components;
+using NameBadge.Models;
 
 namespace NameBadge.Pages;
 
 public partial class Home : ComponentBase
 {
+    [CascadingParameter]
+    private UserSettings UserSettings { get; set; } = new();
 
+    private string DisplayEventData()
+    {
+        var programCode = UserSettings?.ProgramCode;
+        var eventName = UserSettings?.EventName;
+
+        if (string.IsNullOrWhiteSpace(programCode))
+        {
+            return "You must select a program.";
+        }
+
+        if (string.IsNullOrWhiteSpace(eventName))
+        {
+            return "You must supply a specific event name.";
+        }
+
+        var program =
+            ProgramType.ProgramTypes.FirstOrDefault(p =>
+                p.Code.Equals(programCode, StringComparison.OrdinalIgnoreCase));
+        if (program is null)
+        {
+            return "Irrecoverable error.";
+        }
+
+        return $"{program.DisplayName} {eventName}";
+    }
 }
